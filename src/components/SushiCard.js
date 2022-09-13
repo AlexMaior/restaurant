65cf416d1dbfe7abc1600635168ddbacc3e6b44b
Alex Maior <alexmaior@yahoo.com>
2022-09-13 03:38:09 +0300

    added scrolling effects

--- src/components/SushiCard.js
+++ src/components/SushiCard.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Parallax } from "react-scroll-parallax";
+import { ScrollRotate } from "react-scroll-rotate";
 import classes from "./SushiCard.module.css";
 
 const SushiCard = (props) => {
   return (
-    <div className={classes.sushiPopContainer}>
-      <img src={props.img} className={classes.sushiSize} alt="sushi" />
-      <p className={classes.description}>{props.description}</p>
-    </div>
+    <Parallax speed={30}>
+      <div className={classes.sushiPopContainer}>
+        <img src={props.img} className={classes.sushiSize} alt="sushi" />
+        <p className={classes.description}>{props.description}</p>
+      </div>
+    </Parallax>
   );
 };
 
