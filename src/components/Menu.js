b25e12ce54a27276e57c7b967265b787ca3bd399
Alex Maior <alexmaior@yahoo.com>
2022-09-28 10:06:01 +0300

    small change

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 const Menu = () => {
-  return <div>hello</div>;
+  const [results, setResults] = useState([]);
+  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=sushi";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setResults(data.meals));
+  }, []);
+
+  console.log(results);
+
+  return (
+    <div>
+      {results.map((result) => (
+        <p key={result.idMeal}>{result.idMeal}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
